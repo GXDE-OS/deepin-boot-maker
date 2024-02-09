daac1d25d12ac4a22f58e820771425d913405689
gfdgd-xi <3025613752@qq.com>
2024-02-09 07:14:38 +0000

    2.0.4.11

---
+++ src/translations/deepin-boot-maker_bn.ts
@@ -0,0 +1,209 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="bn" version="2.1">
+<context>
+    <name>BMHandler</name>
+    <message>
+        <location filename="../libdbm/backend/bootmaker.cpp" line="64"/>
+        <source>Failed to call the command %1</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../libdbm/backend/bootmaker.cpp" line="66"/>
+        <source>Disk Format Error: Please format the disk with FAT32</source>
+        <translation>ডিস্ক ফরম্যাটে ত্রুটি: দয়াকরে আপনার ডিস্কটি FAT32 দিয়ে ফরম্যাট দিন </translation>
+    </message>
+    <message>
+        <location filename="../libdbm/backend/bootmaker.cpp" line="68"/>
+        <source>Insufficient Disk Space: Ensure the disk has %1 free space</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../libdbm/backend/bootmaker.cpp" line="70"/>
+        <source>Disk Mount Error: Insert the disk again or reboot to retry</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../libdbm/backend/bootmaker.cpp" line="72"/>
+        <source>Image Decompression Error: Verify md5 checksum of the image to ensure its integrity</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../libdbm/backend/bootmaker.cpp" line="74"/>
+        <source>Internal Error</source>
+        <translation>অভ্যন্তরীণ ত্রুটি</translation>
+    </message>
+</context>
+<context>
+    <name>BMWindow</name>
+    <message>
+        <location filename="../app/bmwindow.cpp" line="117"/>
+        <source>Deepin Boot Maker is a simple tool to write system image files into CD/DVD, USB flash drive and other media.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/bmwindow.cpp" line="119"/>
+        <source>Deepin Boot Maker</source>
+        <translation>ডিপিন বুট মেকার</translation>
+    </message>
+</context>
+<context>
+    <name>ISOSelectView</name>
+    <message>
+        <location filename="../app/view/isoselectview.cpp" line="68"/>
+        <source>Drag an ISO image file and drop it here</source>
+        <translation>একটি ISO ফাইল টেনে এনে এখানে ছেড়ে দিন</translation>
+    </message>
+    <message>
+        <location filename="../app/view/isoselectview.cpp" line="72"/>
+        <source>OR</source>
+        <translation>অথবা</translation>
+    </message>
+    <message>
+        <location filename="../app/view/isoselectview.cpp" line="46"/>
+        <location filename="../app/view/isoselectview.cpp" line="84"/>
+        <source>Select an ISO image file</source>
+        <translation>একটি ISO ফাইল নির্বাচন করুন</translation>
+    </message>
+    <message>
+        <location filename="../app/view/isoselectview.cpp" line="102"/>
+        <source>Next</source>
+        <translation>পরবর্তী</translation>
+    </message>
+    <message>
+        <location filename="../app/view/isoselectview.cpp" line="162"/>
+        <source>Reselect an ISO image file</source>
+        <translation>পুনরায় একটি ISO ফাইল নির্বাচন করুন</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressView</name>
+    <message>
+        <location filename="../app/view/progressview.cpp" line="44"/>
+        <source>Making</source>
+        <translation>তৈরী করা হচ্ছে</translation>
+    </message>
+    <message>
+        <location filename="../app/view/progressview.cpp" line="51"/>
+        <source>Making the disk, please wait...</source>
+        <translation>ডিস্কটি তৈরী করা হচ্ছে, দয়াকরে অপেক্ষা করুন</translation>
+    </message>
+    <message>
+        <location filename="../app/view/progressview.cpp" line="54"/>
+        <source>Do not remove the disk or shut down the computer during the process</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/progressview.cpp" line="62"/>
+        <source>Cancel</source>
+        <translation>বাতিল</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../libdbm/util/deviceinfo.h" line="40"/>
+        <source>Removable Disk</source>
+        <translation>অপসারণযোগ্য ডিস্ক</translation>
+    </message>
+    <message>
+        <location filename="../libdbm/util/utils.cpp" line="118"/>
+        <source>Removable disk</source>
+        <translation>অপসারণযোগ্য ডিস্ক</translation>
+    </message>
+    <message>
+        <location filename="../app/main.cpp" line="116"/>
+        <source>Deepin Boot Maker</source>
+        <translation>ডিপিন বুট মেকার</translation>
+    </message>
+</context>
+<context>
+    <name>ResultView</name>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="86"/>
+        <source>Reboot now</source>
+        <translation>এখনি রিবুট করুন</translation>
+    </message>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="79"/>
+        <source>Done</source>
+        <translation>শেষ</translation>
+    </message>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="52"/>
+        <source>Successful</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="122"/>
+        <source>The error log will be uploaded automatically with the feedback. We cannot improve without your feedback</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="123"/>
+        <source>Submit Feedback</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="137"/>
+        <source>Close</source>
+        <translation>বন্ধ</translation>
+    </message>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="145"/>
+        <source>Sorry, making failed</source>
+        <translation>দুঃখিত, তৈরী করতে বার্থ হয়েছে</translation>
+    </message>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="146"/>
+        <source>Making failed</source>
+        <translation>তৈরী করতে বার্থ</translation>
+    </message>
+</context>
+<context>
+    <name>UsbSelectView</name>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="102"/>
+        <source>Formatting disk can increase the making success rate</source>
+        <translation>ডিস্কটি ফরম্যাট করলে তৈরী করার সফলতার হার বাড়তে পারে</translation>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="136"/>
+        <source>Start making</source>
+        <translation> তৈরী করা শুরু করুন</translation>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="231"/>
+        <source>Format USB flash drive</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="79"/>
+        <source>Select a disk</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="124"/>
+        <source>No available disk</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="153"/>
+        <source>Formatting will erase all data on the disk, please confirm and continue</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="233"/>
+        <source>Formatting the disk will overwrite all data, please have a backup before proceeding.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="234"/>
+        <source>Cancel</source>
+        <translation>বাতিল করুন</translation>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="235"/>
+        <source>Ok</source>
+        <translation>ওকে</translation>
+    </message>
+</context>
+</TS>
---
+++ src/translations/deepin-boot-maker_ms.ts
@@ -0,0 +1,209 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="ms" version="2.1">
+<context>
+    <name>BMHandler</name>
+    <message>
+        <location filename="../libdbm/backend/bootmaker.cpp" line="64"/>
+        <source>Failed to call the command %1</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../libdbm/backend/bootmaker.cpp" line="66"/>
+        <source>Disk Format Error: Please format the disk with FAT32</source>
+        <translation>Ralat Format Cakera: Sila format cakera dengan FAT32</translation>
+    </message>
+    <message>
+        <location filename="../libdbm/backend/bootmaker.cpp" line="68"/>
+        <source>Insufficient Disk Space: Ensure the disk has %1 free space</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../libdbm/backend/bootmaker.cpp" line="70"/>
+        <source>Disk Mount Error: Insert the disk again or reboot to retry</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../libdbm/backend/bootmaker.cpp" line="72"/>
+        <source>Image Decompression Error: Verify md5 checksum of the image to ensure its integrity</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../libdbm/backend/bootmaker.cpp" line="74"/>
+        <source>Internal Error</source>
+        <translation>Ralat Dalaman</translation>
+    </message>
+</context>
+<context>
+    <name>BMWindow</name>
+    <message>
+        <location filename="../app/bmwindow.cpp" line="117"/>
+        <source>Deepin Boot Maker is a simple tool to write system image files into CD/DVD, USB flash drive and other media.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/bmwindow.cpp" line="119"/>
+        <source>Deepin Boot Maker</source>
+        <translation>Pembina But Deepin</translation>
+    </message>
+</context>
+<context>
+    <name>ISOSelectView</name>
+    <message>
+        <location filename="../app/view/isoselectview.cpp" line="68"/>
+        <source>Drag an ISO image file and drop it here</source>
+        <translation>Seret fail imej ISO dan lepaskannya di sini</translation>
+    </message>
+    <message>
+        <location filename="../app/view/isoselectview.cpp" line="72"/>
+        <source>OR</source>
+        <translation>ATAU</translation>
+    </message>
+    <message>
+        <location filename="../app/view/isoselectview.cpp" line="46"/>
+        <location filename="../app/view/isoselectview.cpp" line="84"/>
+        <source>Select an ISO image file</source>
+        <translation>Pilih satu fail imej ISO</translation>
+    </message>
+    <message>
+        <location filename="../app/view/isoselectview.cpp" line="102"/>
+        <source>Next</source>
+        <translation>Berikutnya</translation>
+    </message>
+    <message>
+        <location filename="../app/view/isoselectview.cpp" line="162"/>
+        <source>Reselect an ISO image file</source>
+        <translation>Pilih semula satu fail imej ISO</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressView</name>
+    <message>
+        <location filename="../app/view/progressview.cpp" line="44"/>
+        <source>Making</source>
+        <translation>Sedang Menyediakan</translation>
+    </message>
+    <message>
+        <location filename="../app/view/progressview.cpp" line="51"/>
+        <source>Making the disk, please wait...</source>
+        <translation>Menyediakan cakera, tunggu sebentar...</translation>
+    </message>
+    <message>
+        <location filename="../app/view/progressview.cpp" line="54"/>
+        <source>Do not remove the disk or shut down the computer during the process</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/progressview.cpp" line="62"/>
+        <source>Cancel</source>
+        <translation>Batal</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../libdbm/util/deviceinfo.h" line="40"/>
+        <source>Removable Disk</source>
+        <translation>Cakera Boleh Alih</translation>
+    </message>
+    <message>
+        <location filename="../libdbm/util/utils.cpp" line="118"/>
+        <source>Removable disk</source>
+        <translation>Cakera boleh alih</translation>
+    </message>
+    <message>
+        <location filename="../app/main.cpp" line="116"/>
+        <source>Deepin Boot Maker</source>
+        <translation>Pembina But Deepin</translation>
+    </message>
+</context>
+<context>
+    <name>ResultView</name>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="86"/>
+        <source>Reboot now</source>
+        <translation>But semula sekarang</translation>
+    </message>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="79"/>
+        <source>Done</source>
+        <translation>Selesai</translation>
+    </message>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="52"/>
+        <source>Successful</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="122"/>
+        <source>The error log will be uploaded automatically with the feedback. We cannot improve without your feedback</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="123"/>
+        <source>Submit Feedback</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="137"/>
+        <source>Close</source>
+        <translation>Tutup</translation>
+    </message>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="145"/>
+        <source>Sorry, making failed</source>
+        <translation>Maaf, penyediaan gagal</translation>
+    </message>
+    <message>
+        <location filename="../app/view/resultview.cpp" line="146"/>
+        <source>Making failed</source>
+        <translation>Penyediaan gagal</translation>
+    </message>
+</context>
+<context>
+    <name>UsbSelectView</name>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="102"/>
+        <source>Formatting disk can increase the making success rate</source>
+        <translation>Memformat cakera boleh meningkatkan kadar kejayaan pembuatannya</translation>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="136"/>
+        <source>Start making</source>
+        <translation>Mula membuat</translation>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="231"/>
+        <source>Format USB flash drive</source>
+        <translation>Format pemacu kilat USB</translation>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="79"/>
+        <source>Select a disk</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="124"/>
+        <source>No available disk</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="153"/>
+        <source>Formatting will erase all data on the disk, please confirm and continue</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="233"/>
+        <source>Formatting the disk will overwrite all data, please have a backup before proceeding.</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="234"/>
+        <source>Cancel</source>
+        <translation>Batal</translation>
+    </message>
+    <message>
+        <location filename="../app/view/usbselectview.cpp" line="235"/>
+        <source>Ok</source>
+        <translation>Ok</translation>
+    </message>
+</context>
+</TS>
